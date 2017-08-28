7a1c8003a0cf9f0970a9b7c0a4bb0f1c601a34ef
MintSoup <aregak2005@gmail.com>
2017-08-28 12:09:45 +0400

    Things work again

--- src/main/java/me/mintsoup/serial/Handler.java
+++ src/main/java/me/mintsoup/serial/Handler.java
@@ -15,7 +15,6 @@
  *     You should have received a copy of the GNU General Public License
  *     along with MintSoup's Serial Communicator.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package me.mintsoup.serial;
 
 import jssc.SerialPort;
