3d84bfb157b3cd4ce46c4c5834070857fde69d88
MintSoup <aregak2005@gmail.com>
2017-08-28 12:17:16 +0400

    v3

--- src/main/java/me/mintsoup/serial/Main.java
+++ src/main/java/me/mintsoup/serial/Main.java
@@ -1,6 +1,7 @@
 /**
  * Copyright (C) 2017 Areg Hovhannisyan
  * This file is part of MintSoup's Serial Communicator.
+ *
  *     MintSoup's Serial Communicator is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
